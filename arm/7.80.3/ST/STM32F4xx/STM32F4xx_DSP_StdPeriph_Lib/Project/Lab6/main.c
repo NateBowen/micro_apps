4a58ac7c29812a30f04abb7f803b02b1e90eee01
ecelab <ecelab>
2017-02-27 19:50:54 -0500

    Add Task 1, Lab 6

--- arm/7.80.3/ST/STM32F4xx/STM32F4xx_DSP_StdPeriph_Lib/Project/Lab6/main.c
+++ arm/7.80.3/ST/STM32F4xx/STM32F4xx_DSP_StdPeriph_Lib/Project/Lab6/main.c
@@ -1,16 +1,81 @@
 #include "main.h"
 
+#define SIG_PIN         GPIO_Pin_15
+#define DATA_PINS       0xFF00
+
+GPIO_InitTypeDef        GPIO_InitStructure;
+NVIC_InitTypeDef        NVIC_InitStructureMain;
+TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+TIM_OCInitTypeDef       TIM_OCInitStructure;
+
 int main(void)
 {
   /*!< At this stage the microcontroller clock setting is already configured, 
-       this is done through SystemInit() function which is called from startup
-       files (startup_stm32f40xx.s/startup_stm32f427x.s/startup_stm32f429x.s)
-       before to branch to application main. 
-       To reconfigure the default setting of SystemInit() function, refer to
-       system_stm32f4xx.c file
-     */  
+  this is done through SystemInit() function which is called from startup
+  files (startup_stm32f40xx.s/startup_stm32f427x.s/startup_stm32f429x.s)
+  before to branch to application main. 
+  To reconfigure the default setting of SystemInit() function, refer to
+  system_stm32f4xx.c file
+  */  
+  
+  uint16_t ticks = ((SystemCoreClock / 2) / (2500)) - 1;
+  
+  
+  /* Time base default values configuration */
+//  TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
+  
+  /* Output compare default values configuration */
+//  TIM_OCStructInit(&TIM_OCInitStructure);
     
-  /* Infinite loop */
+  // Set up clocks for all peripherals
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
+  
+  // Set up GPIOC port for SIG_PIN
+  GPIO_InitStructure.GPIO_Pin = SIG_PIN;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+  
+  // Set up GPIOD port for DATA_PINS
+  GPIO_InitStructure.GPIO_Pin = DATA_PINS;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+  
+  // Set up TIM5 base configuration
+  TIM_TimeBaseStructure.TIM_Period = 0xFFFF;
+  TIM_TimeBaseStructure.TIM_Prescaler = 0;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseInit(TIM5, &TIM_TimeBaseStructure);
+  
+  // Output Compare Timing Mode configuaration: Channel 1
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_Timing;
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = ticks;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+  TIM_OC1Init(TIM5, &TIM_OCInitStructure);
+    
+  // Set up NVIC
+  NVIC_InitStructureMain.NVIC_IRQChannel = TIM5_IRQn;
+  NVIC_InitStructureMain.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructureMain.NVIC_IRQChannelSubPriority = 1;
+  NVIC_InitStructureMain.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructureMain);
+  
+  // Enable TIM5 interrupts
+  TIM_ITConfig(TIM5, TIM_IT_CC1, ENABLE);
+  
+  // Enable TIM5 counter
+  TIM_Cmd(TIM5, ENABLE);
+  
+  // Infinite loop
   while (1)
   {
     
--- arm/7.80.3/ST/STM32F4xx/STM32F4xx_DSP_StdPeriph_Lib/Project/Lab6/stm32f4xx_it.c
+++ arm/7.80.3/ST/STM32F4xx/STM32F4xx_DSP_StdPeriph_Lib/Project/Lab6/stm32f4xx_it.c
@@ -1,168 +1,34 @@
-/**
-  ******************************************************************************
-  * @file    Project/STM32F4xx_StdPeriph_Template/stm32f4xx_it.c 
-  * @author  MCD Application Team
-  * @version V1.2.0RC2
-  * @date    20-February-2013
-  * @brief   Main Interrupt Service Routines.
-  *          This file provides template for all exceptions handler and 
-  *          peripherals interrupt service routine.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */
-
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_it.h"
 #include "main.h"
 
-/** @addtogroup Template_Project
-  * @{
-  */
+#define SIG_PIN         GPIO_Pin_15
+#define DATA_PINS       0xFF00
 
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-/* Private function prototypes -----------------------------------------------*/
-/* Private functions ---------------------------------------------------------*/
-
-/******************************************************************************/
-/*            Cortex-M4 Processor Exceptions Handlers                         */
-/******************************************************************************/
-
-/**
-  * @brief   This function handles NMI exception.
-  * @param  None
-  * @retval None
-  */
-void NMI_Handler(void)
+void TIM5_IRQHandler(void)
 {
-}
-
-/**
-  * @brief  This function handles Hard Fault exception.
-  * @param  None
-  * @retval None
-  */
-void HardFault_Handler(void)
-{
-  /* Go to infinite loop when Hard Fault exception occurs */
-  while (1)
+  // Check for correct channel interrupt bit
+  if(TIM_GetITStatus(TIM5, TIM_IT_CC1) == SET) 
   {
-  }
+    // Set SIG HIGH
+    GPIOC->ODR = SIG_PIN;
+    
+    // Decrement DATA and write to port
+    GPIOD->ODR--;
+    
+    // Set SIG LOW
+    GPIOC->ODR &= ~SIG_PIN;
+    
+    TIM5->CNT = 0;
+    
+    // Clear interrupt bit    
+    TIM_ClearITPendingBit(TIM5, TIM_IT_CC1);
+  } 
 }
 
-/**
-  * @brief  This function handles Memory Manage exception.
-  * @param  None
-  * @retval None
-  */
-void MemManage_Handler(void)
-{
-  /* Go to infinite loop when Memory Manage exception occurs */
-  while (1)
-  {
-  }
-}
 
-/**
-  * @brief  This function handles Bus Fault exception.
-  * @param  None
-  * @retval None
-  */
-void BusFault_Handler(void)
-{
-  /* Go to infinite loop when Bus Fault exception occurs */
-  while (1)
-  {
-  }
-}
 
-/**
-  * @brief  This function handles Usage Fault exception.
-  * @param  None
-  * @retval None
-  */
-void UsageFault_Handler(void)
-{
-  /* Go to infinite loop when Usage Fault exception occurs */
-  while (1)
-  {
-  }
-}
-
-/**
-  * @brief  This function handles SVCall exception.
-  * @param  None
-  * @retval None
-  */
-void SVC_Handler(void)
-{
-}
-
-/**
-  * @brief  This function handles Debug Monitor exception.
-  * @param  None
-  * @retval None
-  */
-void DebugMon_Handler(void)
-{
-}
-
-/**
-  * @brief  This function handles PendSVC exception.
-  * @param  None
-  * @retval None
-  */
-void PendSV_Handler(void)
-{
-}
-
-/**
-  * @brief  This function handles SysTick Handler.
-  * @param  None
-  * @retval None
-  */
 void SysTick_Handler(void)
 {
   //TimingDelay_Decrement();
 }
-
-/******************************************************************************/
-/*                 STM32F4xx Peripherals Interrupt Handlers                   */
-/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
-/*  available peripheral interrupt handler's name please refer to the startup */
-/*  file (startup_stm32f4xx.s).                                               */
-/******************************************************************************/
-
-/**
-  * @brief  This function handles PPP interrupt request.
-  * @param  None
-  * @retval None
-  */
-/*void PPP_IRQHandler(void)
-{
-}*/
-
-/**
-  * @}
-  */ 
-
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
