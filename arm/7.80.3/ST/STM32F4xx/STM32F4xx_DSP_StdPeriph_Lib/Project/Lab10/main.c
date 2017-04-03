ed5b220e3ef3a1f6c89225f29c15865bdfc3715c
ecelab <ecelab>
2017-04-03 19:38:12 -0400

    Task 1 Part 2

--- arm/7.80.3/ST/STM32F4xx/STM32F4xx_DSP_StdPeriph_Lib/Project/Lab10/main.c
+++ arm/7.80.3/ST/STM32F4xx/STM32F4xx_DSP_StdPeriph_Lib/Project/Lab10/main.c
@@ -1,18 +1,31 @@
 #include "main.h"
 
-int main(void)
-{
+
+uint16_t recieved = 0x12;
   
+int main(void){
   SysTick_Config(SystemCoreClock / 1000);
   
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-  
+
   SPI_InitTypeDef SPI_InitStructure;
   GPIO_InitTypeDef GPIO_InitStructure;
   
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  
+  
   SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
-  SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
+  if ( GPIOA->IDR & GPIO_Pin_8 ) { //IS_MASTER ) {
+    SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
+  } else {
+    SPI_InitStructure.SPI_Mode = SPI_Mode_Slave;
+  }
   SPI_InitStructure.SPI_DataSize = SPI_DataSize_16b;
   SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
   SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;
@@ -36,9 +49,13 @@ int main(void)
   //  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
   //  GPIO_Init(GPIOA, &GPIO_InitStructure);
   
+  // SPI1_NSS CHIP SELECT
   GPIO_PinAFConfig(GPIOA, GPIO_PinSource4, GPIO_AF_SPI1);
+  // SPI1_SCK
   GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_SPI1);
+  // SPI1_MISO
   GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_SPI1);
+  // SPI1_MOSI
   GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1);
   
   //  GPIOA->ODR &= ~GPIO_Pin_4;
@@ -47,14 +64,17 @@ int main(void)
   
   uint16_t count;
   uint16_t dumbDelay = 0;
-  
-  /* Infinite loop */
-  while (1) {
-    for(count = 0; count < 256; count++){
-      
-      while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
-      SPI_I2S_SendData(SPI1, count);
-      for(dumbDelay = 0; dumbDelay < 0xFFF0; dumbDelay++);     
+
+  while (1) {  
+    if ( GPIOA->IDR & GPIO_Pin_8 ) { //IS_MASTER ) {
+        for(count = 0; count < 256; count++){
+          while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
+          SPI_I2S_SendData(SPI1, count);
+          for(dumbDelay = 0; dumbDelay < 0xFFF0; dumbDelay++);     
+        }
+    } else {
+      while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_RXNE) == RESET);
+      recieved = SPI_I2S_ReceiveData(SPI1);
     }
-  }
+    }
 }
