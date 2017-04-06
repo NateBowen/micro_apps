09859a3f43fdb7e04de7eca7fe249b1bf3a9948f
NATESLAPTOP\Nate <nate_bowen@rocketmail.com>
2017-04-05 21:54:56 -0400

    Add defines for LED.

--- arm/7.80.3/ST/STM32F4xx/STM32F4xx_DSP_StdPeriph_Lib/Project/FinalProject/led.c
+++ arm/7.80.3/ST/STM32F4xx/STM32F4xx_DSP_StdPeriph_Lib/Project/FinalProject/led.c
@@ -2,74 +2,31 @@
 #include "delay.h"
 
 void LED_Config(void) {
-  
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
   GPIO_InitTypeDef GPIO_InitStructure;
-  
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+
+  RCC_AHB1PeriphClockCmd(LED_PORT_CLOCK, ENABLE);
+
+  GPIO_InitStructure.GPIO_Pin = INDICATOR_LED_PIN | IR_EMITTER_PIN;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOB, &GPIO_InitStructure);
-  
-  if((GPIOB->IDR & 0X0001) == 0X0001){   
-    
-    GPIOB->BSRRL = GPIO_Pin_3;     // IR EMITTER LED OFF   
-    
-    // CHECK TO SEE IF START DIP SWITCH IS ON
-    for(uint8_t i = 0 ; i<=5 ; i++){
-      
-      GPIOB->BSRRL = GPIO_Pin_2;   // INDICATOR LED ON
-      Delay(500);                  // 1/2 SECOND DELAY
-      
-      GPIOB->BSRRH = GPIO_Pin_2;   // INDICATOR LED OFF
-      Delay(500);                  // 1/2 SECOND DELAY
-      
-      
-      // 1 SECOND TOTAL AS SPECIFIED REPEATED FIVE TIMES 
-      // 1 BLINK PER SECOND
-    }
-  }
-  
-  else if(((GPIOB->IDR & 0X0001) == 0X0001) && ((GPIOB->IDR & 0X0002) == 0X0002)){
-    
-    GPIOB->BSRRH = GPIO_Pin_3;     // IR EMITTER LED OFF
-    // CHECK TO SEE IF START DIP SWITCH IS ON    
-    for(uint8_t i = 0; i<=8 ; i++){
-      
-      GPIOB->BSRRL = GPIO_Pin_2;   // INDICATOR LED ON
-      Delay(250);                  // 1/4 SECOND DELAY
-      
-      GPIOB->BSRRH = GPIO_Pin_2;   // INDICATOR LED OFF
-      Delay(250);                  // 1/4 SECOND DELAY
-      
-      GPIOB->BSRRL = GPIO_Pin_2;   // INDICATOR LED ON
-      Delay(250);                  // 1/4 SECOND DELAY
-      
-      GPIOB->BSRRH = GPIO_Pin_2;   // INDICATOR LED OFF
-      Delay(250);                  // 1/4 SECOND DELAY
-      
-      
-      // 1 SECOND TOTAL AS SPECIFIED REPEATED FIVE TIMES 
-      // 2 BLINKS PER SECOND
-    }
-  }
+  GPIO_Init(LED_PORT, &GPIO_InitStructure);
 }
 
-
 void IndicatorLEDOn(void){
-
+  LED_PORT->ODR |= INDICATOR_LED_PIN;
 }
 
 void IndicatorLEDOff(void){
-  
+  LED_PORT->ODR &= ~INDICATOR_LED_PIN;
 }
 
 void IREmitterOn(void){
-
+  LED_PORT->ODR |= IR_EMITTER_PIN;
 }
 
 void IREmitterOff(void){
-
+  LED_PORT->ODR &= ~IR_EMITTER_PIN;
 }
--- arm/7.80.3/ST/STM32F4xx/STM32F4xx_DSP_StdPeriph_Lib/Project/FinalProject/led.h
+++ arm/7.80.3/ST/STM32F4xx/STM32F4xx_DSP_StdPeriph_Lib/Project/FinalProject/led.h
@@ -1,7 +1,13 @@
 #include "main.h"
 
-#define NUM_START_BLINKS 5
-#define NUM_STOP_BLINKS 8
+#define LED_PORT          GPIOB
+#define LED_PORT_CLOCK    RCC_AHB1Periph_GPIOB
+#define INDICATOR_LED_PIN GPIO_Pin_2
+#define IR_EMITTER_PIN    GPIO_Pin_3
+
+
+#define NUM_START_BLINKS  5
+#define NUM_STOP_BLINKS   8
 
 void LED_Config(void);
 void IndicatorLEDOn(void);
