0564c19bc7eb6ac8c65369dc3f2cf538c4a5b53a
NATESLAPTOP\Nate <nate_bowen@rocketmail.com>
2017-04-06 12:02:43 -0400

    Replace magic numbers with defines.

--- arm/7.80.3/ST/STM32F4xx/STM32F4xx_DSP_StdPeriph_Lib/Project/FinalProject/stm32f4xx_it.c
+++ arm/7.80.3/ST/STM32F4xx/STM32F4xx_DSP_StdPeriph_Lib/Project/FinalProject/stm32f4xx_it.c
@@ -22,35 +22,35 @@ It sets the global encoderFrequency variable.
 uint32_t encoderFrequency;
 
 void TIM5_IRQHandler(void){
-  
+
   static uint16_t TIM5_CC2_sigLevel = 0;
-  static  uint16_t IC2ReadValue1 = 0;
-  static  uint16_t IC2ReadValue2 = 0;
-  static  uint16_t IC2ReadValue3 = 0;
-  static   uint16_t CaptureNumber = 0;
-  static   uint32_t Capture = 0;
-  static   uint32_t Capture2 = 0;
-  
+  static uint16_t IC2ReadValue1 = 0;
+  static uint16_t IC2ReadValue2 = 0;
+  static uint16_t IC2ReadValue3 = 0;
+  static uint16_t CaptureNumber = 0;
+  static uint32_t Capture = 0;
+  static uint32_t Capture2 = 0;
+
   if(TIM_GetITStatus(TIM5, TIM_IT_CC2) == SET) {
     /* Check the signal level, only measure when starting with a peak */
-    TIM5_CC2_sigLevel = (GPIOA->IDR & 0x02) >> 1;
-    
-    if ((CaptureNumber == 0) & (TIM5_CC2_sigLevel == 1)) {
+    TIM5_CC2_sigLevel = (ENCODER_PORT->IDR & ENCODER_PIN);
+
+    if ((CaptureNumber == 0) & (TIM5_CC2_sigLevel)) {
       /* Get the Input Capture value */
       IC2ReadValue1 = TIM_GetCapture2(TIM5);
       CaptureNumber = 1;
     }
-    
+
     else if(CaptureNumber == 1) {
       /* Get the Input Capture value */
       IC2ReadValue2 = TIM_GetCapture2(TIM5);
       CaptureNumber = 2;
     }
-    
+
     else if (CaptureNumber == 2) {
       /* Get the Input Capture value */
       IC2ReadValue3 = TIM_GetCapture2(TIM5);
-      
+
       /* First period computation */
       if (IC2ReadValue2 > IC2ReadValue1) {
         Capture = (IC2ReadValue2 - IC2ReadValue1);
@@ -61,7 +61,7 @@ void TIM5_IRQHandler(void){
       else {
         Capture = 0;
       }
-      
+
       /* Second period computation */
       if (IC2ReadValue3 > IC2ReadValue2) {
         Capture2 = (IC2ReadValue3 - IC2ReadValue2);
@@ -71,10 +71,10 @@ void TIM5_IRQHandler(void){
       }
       else {
         Capture2 = 0;
-      }   
+      }
       /* Frequency computation */
       encoderFrequency = (uint32_t) SystemCoreClock / (2 * (Capture + Capture2));
-      
+
       CaptureNumber = 0;
     }
     /* Clear TIM5 Capture compare interrupt pending bit */
