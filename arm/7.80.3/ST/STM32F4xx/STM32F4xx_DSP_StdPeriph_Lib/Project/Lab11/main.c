85317c269dab4b557f5e47b69298123d5b6e9a12
ecelab <ecelab>
2017-04-10 14:00:16 -0400

    Add Lab11 structure.

---
+++ arm/7.80.3/ST/STM32F4xx/STM32F4xx_DSP_StdPeriph_Lib/Project/Lab11/main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+int main(void)
+{
+  /*!< At this stage the microcontroller clock setting is already configured, 
+       this is done through SystemInit() function which is called from startup
+       files (startup_stm32f40xx.s/startup_stm32f427x.s/startup_stm32f429x.s)
+       before to branch to application main. 
+       To reconfigure the default setting of SystemInit() function, refer to
+       system_stm32f4xx.c file
+     */  
+    
+  /* Infinite loop */
+  while (1)
+  {
+    
+  }
+}
